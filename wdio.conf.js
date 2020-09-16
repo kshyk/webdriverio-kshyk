67a7babb5822e615929013c188db77c925f1a907
kshyk <jj.turkiewicz@gmail.com>
2020-09-16 23:19:34 +0200

    - provided testingbot url

--- wdio.conf.js
+++ wdio.conf.js
@@ -93,7 +93,8 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: "http://localhost",
+    //baseUrl: "http://localhost",
+    baseUrl: "https://" + user + ":" + key + "@hub.testingbot.com/wd/hub"
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
